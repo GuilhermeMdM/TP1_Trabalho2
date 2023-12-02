0e0b63b45c68c4c63887bbd5c4f00150f82bfce9
phbm2003 <145854692+phbm2003@users.noreply.github.com>
2023-12-02 13:24:03 -0300

    emails válidos restritos stubs.cpp

--- stubs.cpp
+++ stubs.cpp
@@ -3,6 +3,14 @@
 //----------------------------------------------------------------------------
 // Implementação de método de classe stub.
 
+
+const string StubIServicoAutenticacao::TRIGGER_FALHA = "bb@okcd7";
+const string StubIServicoAutenticacao::TRIGGER_ERRO_SISTEMA = "aa@okcd7";
+const string StubIServicoControle::TRIGGER_FALHA = "bb@okcd7";
+const string StubIServicoControle::TRIGGER_ERRO_SISTEMA = "aa@okcd7";
+const string StubIServicoProjetos::TRIGGER_FALHA = "bb@okcd7";
+const string StubIServicoProjetos::TRIGGER_ERRO_SISTEMA = "aa@okcd7";
+
 bool StubIServicoAutenticacao::autenticar(const Email &email, const Senha &senha){
 
     // Apresentar dados recebidos.
@@ -17,9 +25,9 @@ bool StubIServicoAutenticacao::autenticar(const Email &email, const Senha &senha
 
     // Diferentes comportamentos dependendo do valor do email.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             throw runtime_error("Erro de sistema.");
     }
     return resultado;
@@ -39,9 +47,9 @@ bool StubIServicoControle::cadastrar(const Conta conta){
 
     // Diferentes comportamentos dependendo do valor do email.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             throw runtime_error("Erro de sistema.");
     }
 
@@ -59,9 +67,9 @@ bool StubIServicoControle::eliminar(const Email email) {
 
     // Diferentes comportamentos dependendo do valor do email.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             throw runtime_error("Erro de sistema.");
     }
 
@@ -79,9 +87,9 @@ bool StubIServicoControle::editar(const Conta conta) {
 
     // Diferentes comportamentos dependendo do valor do email.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             throw runtime_error("Erro de sistema.");
     }
 
@@ -99,9 +107,9 @@ bool StubIServicoControle::visualizar(const Conta* conta) {
 
     // Diferentes comportamentos dependendo do valor do email.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             throw runtime_error("Erro de sistema.");
     }
 
@@ -116,9 +124,9 @@ void StubIServicoProjetos::criarQuadro(const Conta conta) {
     string valor = conta.getEmail().getValor();
     // Diferentes comportamentos dependendo do valor do email.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             cout << "Criar quadro falhou." << endl;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             cout << "Erro de sistema ao criar quadro." << endl;
             throw runtime_error("Erro de sistema.");
     }
@@ -132,9 +140,9 @@ void StubIServicoProjetos::visualizarQuadro(const Quadro* quadro) {
     string valor = quadro->getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             cout << "Visualizar quadro falhou." << endl;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             cout << "Erro de sistema ao visualizar quadro." << endl;
             throw runtime_error("Erro de sistema.");
     }
@@ -148,9 +156,9 @@ void StubIServicoProjetos::eliminarQuadro(const Quadro quadro) {
     string valor = quadro.getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             cout << "Eliminar quadro falhou." << endl;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             cout << "Erro de sistema ao eliminar quadro." << endl;
             throw runtime_error("Erro de sistema.");
     }
@@ -164,9 +172,9 @@ void StubIServicoProjetos::criarCartao(const Quadro quadro) {
     string valor = quadro.getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             cout << "Criar cartão falhou." << endl;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             cout << "Erro de sistema ao criar cartão." << endl;
             throw runtime_error("Erro de sistema.");
     }
@@ -180,9 +188,9 @@ void StubIServicoProjetos::visualizarCartao(const Cartao* cartao) {
     string valor = cartao->getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             cout << "Visualizar cartão falhou." << endl;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             cout << "Erro de sistema ao visualizar cartão." << endl;
             throw runtime_error("Erro de sistema.");
     }
@@ -196,9 +204,9 @@ void StubIServicoProjetos::moverCartao(Coluna coluna) {
     string valor = coluna.getValor();
     // Diferentes comportamentos dependendo do valor da coluna.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             cout << "Mover cartão falhou." << endl;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             cout << "Erro de sistema ao mover cartão." << endl;
             throw runtime_error("Erro de sistema.");
     }
@@ -212,9 +220,9 @@ void StubIServicoProjetos::eliminarCartao(const Cartao cartao) {
     string valor = cartao.getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
-    if (valor == to_string(TRIGGER_FALHA)) {
+    if (valor == TRIGGER_FALHA) {
             cout << "Eliminação do cartão falhou." << endl;
-    } else if (valor == to_string(TRIGGER_ERRO_SISTEMA)) {
+    } else if (valor == TRIGGER_ERRO_SISTEMA) {
             cout << "Erro de sistema ao eliminar o cartão." << endl;
             throw runtime_error("Erro de sistema.");
     }
