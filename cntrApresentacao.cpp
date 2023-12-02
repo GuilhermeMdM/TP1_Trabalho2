6bbded9cd82812bfa4c6f5dbefd4a2e38d5a21c5
phbm2003 <145854692+phbm2003@users.noreply.github.com>
2023-12-02 20:48:48 -0300

    Update cntrApresentacao.cpp

--- cntrApresentacao.cpp
+++ cntrApresentacao.cpp
@@ -80,9 +80,10 @@ void CntrApresentacaoInicializacao::executar(){
 
 bool CntrApresentacaoAutenticacao::autenticar(Email* email){
 
-    char texto1[]="Digite o e-mail  : ";
+    char texto1[]="Digite o e-mail: ";
     char texto2[]="Digite a senha: ";
     char texto3[]="Dado em formato incorreto. Digite algo.";
+
 
     char campo1[80];
     char campo2[80];
@@ -98,6 +99,7 @@ bool CntrApresentacaoAutenticacao::autenticar(Email* email){
         cin >> campo1;
         cout << texto2 << " ";
         cin >> campo2;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         try{
             email->setValor(string(campo1));
@@ -113,6 +115,73 @@ bool CntrApresentacaoAutenticacao::autenticar(Email* email){
     return (cntr->autenticar(*email, senha));
 }
 
+void CntrApresentacaoControle::cadastrar(){
+
+    // Mensagens a serem apresentadas na tela de cadastramento.
+
+    char texto0[] ="Preencha os seguintes campos: ";
+    char texto1[]="Nome: ";
+    char texto2[]="E-mail: ";
+    char texto3[]="Senha: ";
+    char texto4[]="Dados em formato incorreto. Digite algo.";
+    char texto5[]="Sucesso no cadastramento. Digite algo.";
+    char texto6[]="Falha no cadastramento. Digite algo.";
+
+    char campo1[80],  campo2[80],  campo3[80];      // Cria campos para entrada dos dados.
+
+    // Instanciar os domínios.
+    Texto nome;
+    Email email;
+    Senha senha;
+
+    // Apresenta tela de cadastramento.
+
+    CLR_SCR;                                                                                // Limpa janela.
+
+    cout << texto0 << endl;
+    cout << texto1 << " ";
+    cin.getline(campo1,sizeof(campo1));
+
+
+    cout << texto2 << " ";
+    cin.getline(campo2,sizeof(campo2));                                                     // Imprime nome do campo.
+                                                                                            // Lê valor do campo.
+    cout << texto3 << " ";
+    cin.getline(campo3,sizeof(campo3));                                                     // Imprime nome do campo.
+
+    try{
+        nome.setValor(string(campo1));
+        email.setValor(string(campo2));
+        senha.setValor(string(campo3));
+    }
+    catch(invalid_argument &exp){
+        cout << texto4 << endl;                                                              // Informa formato incorreto.
+        getch();                                                                             // Leitura de caracter digitado.
+        return;
+    }
+
+    // Instancia e inicializa entidades.
+
+    conta.setNome(nome);
+    conta.setEmail(email);
+    conta.setSenha(senha);
+
+    // Cadastra usuário e conta.
+
+    if(cntr->cadastrar(conta)){
+        CLR_SCR;
+        cout << texto5 << endl;                                                              // Informa sucesso.
+        getch();
+        return;
+    }
+
+    CLR_SCR;
+    cout << texto6 << endl;                                                                  // Informa falha.
+    getch();
+
+    return;
+}
+
 void CntrApresentacaoControle::executar(Email email){
 
     char texto1[]="Selecione um dos servicos : ";
@@ -125,6 +194,8 @@ void CntrApresentacaoControle::executar(Email email){
     char texto7[]="4 - Voltar.";
 
     char texto8[]="3 - Voltar.";
+    char texto9[]="Sucesso no método requerido. Digite algo.";
+    char texto10[]="Falha no método requerido. Digite algo.";
 
     int campo;
 
@@ -142,7 +213,7 @@ void CntrApresentacaoControle::executar(Email email){
         campo = getch() - 48;                                                                   // Leitura do campo de entrada e conversao de ASCII.
 
         switch(campo){
-            case 1: cntr->cadastrar(conta);
+            case 1: cntr->cadastrar(conta); 
                 break;
             case 2:
 
@@ -157,10 +228,45 @@ void CntrApresentacaoControle::executar(Email email){
                 campo = getch() - 48;
 
                 switch(campo){
-                    case 1: cntr->eliminar(email);
-                    case 2: cntr->editar(conta);
-                    case 3: cntr->visualizar(&conta);
-                    case 4: break;
+
+                    case 1:
+                        if (cntr->eliminar(email)) {
+                            CLR_SCR;
+                            cout << texto9 << endl;
+                            getch();
+                        } else {
+                            CLR_SCR;
+                            cout << texto10 << endl;
+                            getch();
+                        }
+                        break;
+
+                    case 2:
+                        if(cntr->editar(conta)) {
+                            CLR_SCR;
+                            cout << texto9 << endl;
+                            getch();
+                        } else {
+                            CLR_SCR;
+                            cout << texto10 << endl;
+                            getch();
+                        }
+                        break;
+
+                    case 3:
+                        if(cntr->visualizar(&conta)) {
+                            CLR_SCR;
+                            cout << texto9 << endl;
+                            getch();
+                        } else {
+                            CLR_SCR;
+                            cout << texto10 << endl;
+                            getch();
+                        }
+                        break;
+
+                    case 4:
+                        break;
                 }
                 break;
             case 3:
