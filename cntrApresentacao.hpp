cf422c81312ed8934022f272fb04ba9b5c0af2ba
phbm2003 <145854692+phbm2003@users.noreply.github.com>
2023-12-01 23:37:54 -0300

    MAI cntrApresentacao.hpp

--- cntrApresentacao.hpp
+++ cntrApresentacao.hpp
@@ -14,6 +14,32 @@
 
 using namespace std;
 
+//Autor: 211068790
+class CntrApresentacaoInicializacao{
+    private:
+        Email email;
+        IApresentacaoAutenticacao *cntrIApresentacaoAutenticacao;
+        IApresentacaoControle *cntrIApresentacaoControle;
+        IApresentacaoProjetos *cntrIApresentacaoProjetos;
+    public:
+        void executar();
+        void setCntrIApresentacaoAutenticacao(IApresentacaoAutenticacao*);
+        void setCntrIApresentacaoControle(IApresentacaoControle*);
+        void setCntrIApresentacaoProjetos(IApresentacaoProjetos*);
+};
+
+inline void CntrIApresentacaoInicializacao::setCntrIApresentacaoAutenticacao(IApresentacaoAutenticacao *cntr){
+    cntrIApresentacaoAutenticacao = cntr;
+}
+
+inline void CntrApresentacaoInicializacao::setCntrIApresentacaoControle(IApresentacaoControle *cntr){
+    cntrIApresentacaoControle = cntr;
+}
+
+inline void CntrApresentacaoInicializacao::setCntrIApresentacaoProjetos(IApresentacaoProjetos *cntr){
+    cntrIApresentacaoProjetos = cntr;
+}
+
 //Autor: 221006431
 class CntrApresentacaoAutenticacao:public IApresentacaoAutenticacao{
     private:
