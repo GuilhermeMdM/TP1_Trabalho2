c26fbf6270d5dadb24430898ca9792e590aa1bb5
phbm2003 <145854692+phbm2003@users.noreply.github.com>
2023-12-02 20:40:38 -0300

    Update stubs.cpp

--- stubs.cpp
+++ stubs.cpp
@@ -5,20 +5,22 @@
 
 
 const string StubIServicoAutenticacao::TRIGGER_FALHA = "bb@okcd7";
-const string StubIServicoAutenticacao::TRIGGER_ERRO_SISTEMA = "aa@okcd7";
-const string StubIServicoControle::TRIGGER_FALHA = "bb@okcd7";
-const string StubIServicoControle::TRIGGER_ERRO_SISTEMA = "aa@okcd7";
-const string StubIServicoProjetos::TRIGGER_FALHA = "bb@okcd7";
-const string StubIServicoProjetos::TRIGGER_ERRO_SISTEMA = "aa@okcd7";
+const string StubIServicoControle::TRIGGER_FALHA = "aa@okcd7";
+const string StubIServicoProjetos::TRIGGER_FALHA = "cc@okcd7";
 
 bool StubIServicoAutenticacao::autenticar(const Email &email, const Senha &senha){
 
     // Apresentar dados recebidos.
 
-    cout << endl << "StubIServicoAutenticacao::autenticar" << endl ;
+    CLR_SCR;
 
-    cout << "Email = " << email.getValor() << endl ;
+    cout << "StubIServicoAutenticacao::autenticar" << endl ;
+
+    cout << endl << "Email = " << email.getValor() << endl ;
     cout << "Senha = " << senha.getValor() << endl ;
+
+    cout << endl << "Digite algo."<< endl ;
+    getch();
 
     bool resultado = true;
     string valor = email.getValor();
@@ -26,9 +28,7 @@ bool StubIServicoAutenticacao::autenticar(const Email &email, const Senha &senha
     // Diferentes comportamentos dependendo do valor do email.
 
     if (valor == TRIGGER_FALHA) {
-            resultado = false;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            throw runtime_error("Erro de sistema.");
+        resultado = false;
     }
     return resultado;
 }
@@ -40,7 +40,15 @@ bool StubIServicoControle::cadastrar(const Conta conta){
 
     // Apresentar dados recebidos.
 
-    cout << endl << "StubIServicoControle::cadastrar" << endl ;
+    CLR_SCR;
+
+    cout << "StubIServicoControle::cadastrar" << endl ;
+
+    cout << endl << "Nome = " << conta.getNome().getValor() << endl ;
+    cout << "Email = " << conta.getEmail().getValor() << endl ;
+    cout << "Senha = " << conta.getSenha().getValor() << endl ;
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 
     bool resultado = true;
     string valor = conta.getEmail().getValor();
@@ -49,10 +57,7 @@ bool StubIServicoControle::cadastrar(const Conta conta){
 
     if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            throw runtime_error("Erro de sistema.");
     }
-
     return resultado;
 }
 
@@ -60,7 +65,13 @@ bool StubIServicoControle::eliminar(const Email email) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoControle::eliminar" << endl ;
+
+    cout << endl << "Email = " << email.getValor() << endl ;
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 
     bool resultado = true;
     string valor = email.getValor();
@@ -69,8 +80,6 @@ bool StubIServicoControle::eliminar(const Email email) {
 
     if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            throw runtime_error("Erro de sistema.");
     }
 
     return resultado;
@@ -80,7 +89,15 @@ bool StubIServicoControle::editar(const Conta conta) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoControle::editar" << endl ;
+
+    cout << endl << "Nome = " << conta.getNome().getValor() << endl ;
+    cout << "Email = " << conta.getEmail().getValor() << endl ;
+    cout << "Senha = " << conta.getSenha().getValor() << endl ;
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 
     bool resultado = true;
     string valor = conta.getEmail().getValor();
@@ -89,8 +106,6 @@ bool StubIServicoControle::editar(const Conta conta) {
 
     if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            throw runtime_error("Erro de sistema.");
     }
 
     return resultado;
@@ -100,7 +115,15 @@ bool StubIServicoControle::visualizar(const Conta* conta) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoControle::visualizar" << endl ;
+
+    cout << endl << "Nome = " << conta->getNome().getValor() << endl ;
+    cout << "Email = " << conta->getEmail().getValor() << endl ;
+    cout << "Senha = " << conta->getSenha().getValor() << endl ;
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 
     bool resultado = true;
     string valor = conta->getEmail().getValor();
@@ -109,10 +132,7 @@ bool StubIServicoControle::visualizar(const Conta* conta) {
 
     if (valor == TRIGGER_FALHA) {
             resultado = false;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            throw runtime_error("Erro de sistema.");
     }
-
     return resultado;
 }
 
@@ -120,110 +140,131 @@ void StubIServicoProjetos::criarQuadro(const Conta conta) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoProjetos::criarQuadro" << endl ;
     string valor = conta.getEmail().getValor();
     // Diferentes comportamentos dependendo do valor do email.
 
     if (valor == TRIGGER_FALHA) {
-            cout << "Criar quadro falhou." << endl;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            cout << "Erro de sistema ao criar quadro." << endl;
-            throw runtime_error("Erro de sistema.");
+        cout << "Criar quadro falhou." << endl;
+    } else {
+        cout << "Criar quadro foi um sucesso." << endl;
     }
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 }
 
 void StubIServicoProjetos::visualizarQuadro(const Quadro* quadro) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoProjetos::visualizarQuadro" << endl ;
     string valor = quadro->getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
     if (valor == TRIGGER_FALHA) {
-            cout << "Visualizar quadro falhou." << endl;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            cout << "Erro de sistema ao visualizar quadro." << endl;
-            throw runtime_error("Erro de sistema.");
+        cout << "Visualizar quadro falhou." << endl;
+    } else {
+        cout << "Visualizar quadro foi um sucesso." << endl;
     }
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 }
 
 void StubIServicoProjetos::eliminarQuadro(const Quadro quadro) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoProjetos::eliminarQuadro" << endl ;
     string valor = quadro.getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
     if (valor == TRIGGER_FALHA) {
-            cout << "Eliminar quadro falhou." << endl;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            cout << "Erro de sistema ao eliminar quadro." << endl;
-            throw runtime_error("Erro de sistema.");
+        cout << "Eliminar quadro falhou." << endl;
+    } else {
+        cout << "Eliminar quadro foi um sucesso." << endl;
     }
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 }
 
 void StubIServicoProjetos::criarCartao(const Quadro quadro) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoProjetos::criarCartao" << endl ;
     string valor = quadro.getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
     if (valor == TRIGGER_FALHA) {
-            cout << "Criar cartão falhou." << endl;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            cout << "Erro de sistema ao criar cartão." << endl;
-            throw runtime_error("Erro de sistema.");
+        cout << "Criar cartão falhou." << endl;
+    } else {
+        cout << "Criar cartão foi um sucesso." << endl;
     }
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 }
 
 void StubIServicoProjetos::visualizarCartao(const Cartao* cartao) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoProjetos::visualizarCartao" << endl ;
     string valor = cartao->getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
     if (valor == TRIGGER_FALHA) {
-            cout << "Visualizar cartão falhou." << endl;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            cout << "Erro de sistema ao visualizar cartão." << endl;
-            throw runtime_error("Erro de sistema.");
+        cout << "Visualizar cartão falhou." << endl;
+    } else {
+        cout << "Visualizar cartão foi um sucesso." << endl;
     }
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 }
 
 void StubIServicoProjetos::moverCartao(Coluna coluna) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoProjetos::moverCartao" << endl ;
     string valor = coluna.getValor();
     // Diferentes comportamentos dependendo do valor da coluna.
 
     if (valor == TRIGGER_FALHA) {
-            cout << "Mover cartão falhou." << endl;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            cout << "Erro de sistema ao mover cartão." << endl;
-            throw runtime_error("Erro de sistema.");
+        cout << "Mover cartão falhou." << endl;
+    } else {
+        cout << "Mover cartão foi um sucesso." << endl;
     }
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 }
 
 void StubIServicoProjetos::eliminarCartao(const Cartao cartao) {
 
     // Apresentar dados recebidos.
 
+    CLR_SCR;
+
     cout << endl << "StubIServicoProjetos::eliminarCartao" << endl ;
     string valor = cartao.getCodigo().getValor();
     // Diferentes comportamentos dependendo do valor do código.
 
     if (valor == TRIGGER_FALHA) {
-            cout << "Eliminação do cartão falhou." << endl;
-    } else if (valor == TRIGGER_ERRO_SISTEMA) {
-            cout << "Erro de sistema ao eliminar o cartão." << endl;
-            throw runtime_error("Erro de sistema.");
+        cout << "Eliminação do cartão falhou." << endl;
+    } else {
+        cout << "Eliminação do cartão foi um sucesso." << endl;
     }
+    cout << endl << "Digite algo pra continuar." << endl ;
+    getch();
 }
